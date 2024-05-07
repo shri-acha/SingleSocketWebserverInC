2e68a1c0c6f3f28e98b63a3890a93b0f08ee91ea
shri-acha <happy.irhs@gmail.com>
2024-05-07 21:46:02 +0545

    ADD:gitignore file

--- src/main.c
+++ src/main.c
@@ -8,14 +8,15 @@
 void main(){
 
 	int server_fd;
-	struct sockaddr_in server_addr; //Is almost sockaddr but uses 
+	struct sockaddr_in server_addr; //sockaddr_in is almost sockaddr but is used for ip4 addresses,and has padding with zeros to make the the memory
+					//Equal to the sockaddr 
 
-	if((server_fd = socket(AF_INET6,SOCK_STREAM,0)) < 0){
+	if((server_fd = socket(AF_INET,SOCK_STREAM,0)) < 0){
 		perror("Error occured in starting the socket");
 		exit(EXIT_FAILURE);
 	}
 
-	server_addr.sin_family = AF_INET6; //Assigns the family of IP address to ipv4
+	server_addr.sin_family = AF_INET; //Assigns the family of IP address to ipv4
 	server_addr.sin_addr.s_addr = INADDR_ANY; //assigns INADDR_ANY(all IP assigned to machine) 
 	server_addr.sin_port = htons(PORT); //converts from host byte order into network byte order and assigns to sin(Socket Input taker)_port.
 					    
@@ -23,11 +24,12 @@ void main(){
 
 	if(bind(server_fd,
 		(struct sockaddr *)&server_addr,
-		sizeof(server_addr)) < 0) //bind requires the arguments of the empty server file descriptor(which is an integer value that stores and represents the server and requires the socket address information{Type is casted to convert sockaddr[more standard storage]}). 
-					  //Checkout the documentation for sockaddr_in struct declaration,which has padding to have same storage as of sockaddr 
+		sizeof(server_addr)) < 0) 
+		//bind requires the arguments of the empty server file descriptor(which is an integer
+		//value that stores and represents the server and 
+		//requires the socket address information{Type is casted to convert sockaddr[more standard storage]}). 
+		//Checkout the documentation for sockaddr_in struct declaration,which has padding to have same storage as of sockaddr 
 	{
-printf("%d",server_fd);
-
 		printf("%d",server_fd);
 		perror("Bind Failure!");
 		exit(EXIT_FAILURE);
@@ -47,13 +49,11 @@ printf("%d",server_fd);
 		if(*client_fd = accept(server_fd,
 					(struct sockaddr*)&client_addr,
 					&client_addr_len)<0) //It assigns the pointer to integer, client_fd an integer address returned by accept.
-							     //Look up the syntax for accept() but probably the server_addr(accept the socket request to),client_addr stores the socket information,and size of client_addr is necessary for the acceptance of the request.
+							     //Look up the syntax for accept() but probably the server_addr(accept the socket request to)
+							     //client_addr stores the socket information,and size of client_addr is necessary for the acceptance of the request.
 		{
-			 
 			perror("Error in accepting");	
-
 		}
-
 
 
 	}
